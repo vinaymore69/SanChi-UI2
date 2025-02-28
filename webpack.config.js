7025b874da158383d6e6442ed80ae135d1c02959
Vinay More <vinaymore0110@gmail.com>
2025-02-28 16:22:20 +0530

    hello

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './public/files/src/videoplayer.js',
-  output: {
-    filename: 'videoplayer.js',
-    path: path.join(__dirname, 'public', 'files', 'dist'),
-  },
-};
